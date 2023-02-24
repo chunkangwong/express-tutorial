a581a31107dbe78f4f05b3fe28e889b9416f19c8
Wong Chun Kang <chunkang.wong94@gmail.com>
2023-02-24 19:52:34 +0800

    Integrated pgClient query in posts.router.js

--- src/routers/posts.router.js
+++ src/routers/posts.router.js
@@ -1,15 +1,16 @@
 const express = require("express");
 const z = require("zod");
+const client = require("../pgClient");
 
 const postSchema = z.object({
   title: z.string({
-    required_error: "Title is required",
+    required_error: "title is required",
   }),
   body: z.string({
-    required_error: "Body is required",
+    required_error: "body is required",
   }),
-  userId: z.number({
-    required_error: "UserId is required",
+  user_id: z.number({
+    required_error: "user_id is required",
   }),
 });
 
@@ -21,72 +22,82 @@ const validate = (schema) => async (req, res, next) => {
   next();
 };
 
-const posts = [
-  {
-    userId: 1,
-    id: 1,
-    title:
-      "sunt aut facere repellat provident occaecati excepturi optio reprehenderit",
-    body: "quia et suscipit\nsuscipit recusandae consequuntur expedita et cum\nreprehenderit molestiae ut ut quas totam\nnostrum rerum est autem sunt rem eveniet architecto",
-  },
-  {
-    userId: 1,
-    id: 2,
-    title: "qui est esse",
-    body: "est rerum tempore vitae\nsequi sint nihil reprehenderit dolor beatae ea dolores neque\nfugiat blanditiis voluptate porro vel nihil molestiae ut reiciendis\nqui aperiam non debitis possimus qui neque nisi nulla",
-  },
-];
-
 const router = express.Router();
 
-router.get("", (req, res) => {
-  res.json(posts);
+router.get("", async (req, res) => {
+  try {
+    const { rows: posts } = await client.query("SELECT * FROM public.posts");
+    res.json(posts);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ message: "Internal Server Error" });
+  }
 });
 
-router.get("/:id", (req, res) => {
+router.get("/:id", async (req, res) => {
   const { id } = req.params;
-  const post = posts.find((post) => post.id === parseInt(id));
-  if (!post) {
+  const { rows: posts } = await client.query(
+    "SELECT * FROM public.posts WHERE id = $1",
+    [id]
+  );
+  if (posts.length === 0) {
     res.status(404).json({ message: "Post not found" });
   }
-  res.json(post);
+  res.json(posts[0]);
 });
 
-router.post("/", validate(postSchema), (req, res) => {
-  const post = req.body;
-  post.id = Math.max(...posts.map((post) => post.id)) + 1;
-  posts.push(post);
-  res.json(post);
+router.post("/", validate(postSchema), async (req, res) => {
+  const { title, body, user_id } = req.body;
+  try {
+    const { rows: addedPosts } = await client.query(
+      "INSERT INTO public.posts (title, body, user_id) VALUES ($1, $2, $3) RETURNING *",
+      [title, body, user_id]
+    );
+    res.json(addedPosts);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ message: "Internal Server Error" });
+  }
 });
 
-router.put("/:id", (req, res) => {
+router.put("/:id", async (req, res) => {
   const { id } = req.params;
-  const post = posts.find((post) => post.id === parseInt(id));
-  if (!post) {
-    res.status(404).json({ message: "Post not found" });
+  const { title, body, user_id } = req.body;
+  try {
+    const { rows: oldPosts } = await client.query(
+      "SELECT * FROM public.posts WHERE id = $1",
+      [id]
+    );
+    if (oldPosts.length === 0) {
+      res.status(404).json({ message: "Post not found" });
+    }
+    if (title) {
+      oldPosts[0].title = title;
+    }
+    if (body) {
+      oldPosts[0].body = body;
+    }
+    if (user_id) {
+      oldPosts[0].user_id = user_id;
+    }
+    const { rows: updatedPosts } = await client.query(
+      "UPDATE public.posts SET title = $1, body = $2, user_id = $3 WHERE id = $4 RETURNING *",
+      [oldPosts[0].title, oldPosts[0].body, oldPosts[0].user_id, id]
+    );
+    res.json(updatedPosts);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ message: "Internal Server Error" });
   }
-  const { title, body, userId } = req.body;
-  if (title) {
-    post.title = title;
-  }
-  if (body) {
-    post.body = body;
-  }
-  if (userId) {
-    post.userId = userId;
-  }
-  res.json(post);
 });
 
-router.delete("/:id", (req, res) => {
+router.delete("/:id", async (req, res) => {
   const { id } = req.params;
-  const postIndex = posts.findIndex((post) => post.id === parseInt(id));
-  if (postIndex === -1) {
-    res.status(404).json({ message: "Post not found" });
-  }
-  const post = posts[postIndex];
-  posts.splice(postIndex, 1);
-  res.json(post);
+  const { rows: deletedPosts } = await client.query(
+    "DELETE FROM public.posts WHERE id = $1 returning *",
+    [id]
+  );
+  res.json(deletedPosts);
 });
 
 module.exports = router;
