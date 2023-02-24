29c5bbb769893d3cc2f3fa4c2f8e2f5f4e8f216d
Wong Chun Kang <chunkang.wong94@gmail.com>
2023-02-24 18:31:30 +0800

    Implemented pg

--- src/index.js
+++ src/index.js
@@ -1,21 +1,24 @@
 const express = require("express");
-const dotenv = require("dotenv");
+require("dotenv").config();
 const postsRouter = require("./routers/posts.router");
 
-dotenv.config();
+const client = require("./pgClient");
 
 const PORT = process.env.PORT || 3000;
 
-const app = express();
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+(async () => {
+  await client.connect();
+  const app = express();
+  app.use(express.json());
+  app.use(express.urlencoded({ extended: true }));
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
-});
+  app.get("/", (req, res) => {
+    res.send("Hello World!");
+  });
 
-app.use("/posts", postsRouter);
+  app.use("/posts", postsRouter);
 
-app.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}`);
-});
+  app.listen(PORT, () => {
+    console.log(`Server running on http://localhost:${PORT}`);
+  });
+})();
