325d14e8f3b3be94587691a4176c697466501daa
Wong Chun Kang <chunkang.wong94@gmail.com>
2023-02-25 20:34:14 +0800

    Enhanced error handling

--- src/routers/posts.router.js
+++ src/routers/posts.router.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const z = require("zod");
 const prisma = require("../prisma");
+const Prisma = require("@prisma/client");
 
 const postSchema = z.object({
   title: z.string({
@@ -17,7 +18,9 @@ const postSchema = z.object({
 const validate = (schema) => async (req, res, next) => {
   const { error } = schema.safeParse(req.body);
   if (error) {
-    return res.status(400).json(JSON.parse(error.message));
+    const zError = JSON.parse(error.message);
+    console.log(zError);
+    return res.status(400).json(zError.map((err) => err.message).join("; "));
   }
   next();
 };
@@ -27,24 +30,29 @@ const router = express.Router();
 router.get("/", async (req, res) => {
   try {
     const posts = await prisma.posts.findMany();
-    res.json(posts);
+    return res.json(posts);
   } catch (error) {
     console.log(error);
-    res.status(500).json({ message: "Internal Server Error" });
+    return res.status(500).json({ message: "Internal Server Error" });
   }
 });
 
 router.get("/:id", async (req, res) => {
   const { id } = req.params;
-  const post = await prisma.posts.findUnique({
-    where: {
-      id: Number(id),
-    },
-  });
-  if (!post) {
-    res.status(404).json({ message: "Post not found" });
+  try {
+    const post = await prisma.posts.findUnique({
+      where: {
+        id: Number(id),
+      },
+    });
+    if (!post) {
+      return res.status(404).json({ message: "Post not found" });
+    }
+    return res.json(post);
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({ message: "Internal Server Error" });
   }
-  res.json(post);
 });
 
 router.post("/", validate(postSchema), async (req, res) => {
@@ -57,10 +65,10 @@ router.post("/", validate(postSchema), async (req, res) => {
         user_id,
       },
     });
-    res.json(addedPost);
+    return res.json(addedPost);
   } catch (error) {
     console.log(error);
-    res.status(500).json({ message: "Internal Server Error" });
+    return res.status(500).json({ message: "Internal Server Error" });
   }
 });
 
@@ -78,21 +86,36 @@ router.put("/:id", async (req, res) => {
         user_id,
       },
     });
-    res.json(updatedPost);
+    return res.json(updatedPost);
   } catch (error) {
     console.log(error);
-    res.status(500).json({ message: "Internal Server Error" });
+    if (error instanceof Prisma.PrismaClientKnownRequestError) {
+      if (error.code === "P2025") {
+        return res.status(404).json({ message: "Post not found" });
+      }
+    }
+    return res.status(500).json({ message: "Internal Server Error" });
   }
 });
 
 router.delete("/:id", async (req, res) => {
   const { id } = req.params;
-  const deletedPost = await prisma.posts.delete({
-    where: {
-      id: Number(id),
-    },
-  });
-  res.json(deletedPost);
+  try {
+    const deletedPost = await prisma.posts.delete({
+      where: {
+        id: Number(id),
+      },
+    });
+    return res.json(deletedPost);
+  } catch (error) {
+    console.log(error);
+    if (error instanceof Prisma.PrismaClientKnownRequestError) {
+      if (error.code === "P2025") {
+        return res.status(404).json({ message: "Post not found" });
+      }
+    }
+    return res.status(500).json({ message: "Internal Server Error" });
+  }
 });
 
 module.exports = router;
